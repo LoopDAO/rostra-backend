d28c1482b1b3bf7f5346d2db919523c5a0780bb3
HA <>
2022-06-09 12:17:22 +0800

    refactor: delete code that do not use for now

--- models.py
+++ models.py
@@ -1,25 +1,5 @@
-import email
-from cgi import test
-from sqlite3 import Timestamp
-from unicodedata import name
-
 import mongoengine as me
-from mongoengine import (BooleanField, DateTimeField, IntField, ListField,
-                         LongField, ReferenceField, StringField)
-from rsa import PublicKey
-
-
-class Guild(me.Document):
-    guild_id = StringField()
-    name = StringField()
-    desc = StringField()
-    creator = StringField()
-    signature = StringField()
-    ipfsAddr = StringField()
-
-    def __repr__(self):
-        return "%s , %s , %s , %s\n " % (self.guild_id, self.name, self.desc, self.creator)
-
+from mongoengine import (IntField, StringField)
 
 class Nft(me.Document):
     name = StringField()
@@ -39,71 +19,3 @@ class MintNftDefine(me.Document):
     cotaId = StringField()
     signature=StringField()
     txHash=StringField()
-#---------------------------------------------------------------------------------
-#rule
-class RuleCondition(me.Document):
-    x = StringField()
-    of = StringField()
-
-
-class RuleAction(me.EmbeddedDocument):
-    type = StringField()
-    url = StringField()
-    condition = ReferenceField(RuleCondition)
-    start_time = StringField()
-    end_time = StringField()
-
-    def __repr__(self):
-        return "%s , %s , %s , %s\n " % (self.type, self.url, self.action, self.start_time)
-
-
-class Rule(me.Document):
-    rule_id = StringField()
-    name = StringField()
-    desc = StringField()
-    creator = StringField()
-    signature = StringField()
-    ipfsAddr = StringField()
-    action = me.EmbeddedDocumentField(RuleAction)
-    nft = StringField()
-    finished = BooleanField()
-    timestamp=StringField()
-
-    def __repr__(self):
-        return "%s , %s , %s , %s\n " % (self.id, self.name, self.desc, self.creator)
-
-
-#---------------------------------------------------------------------------------
-#Runner
-class RunnerCondition(me.Document):
-    address = StringField()
-
-
-class RuleResult(me.Document):  # Runner运行后的结果
-    rule_id = StringField()
-    rule_name = StringField()
-    rule_creator = StringField()
-    # address_list_id ->> _id of address list (AddressList)
-    address_list_id = StringField()
-
-
-class AddressList(me.Document):  # Runner运行后获得的address list
-    result_id = StringField()
-    list = ListField(StringField())
-#---------------------------------------------------------------------------------
-class GithubCommit(me.Document):
-    message=StringField()
-    sha=StringField()
-    author=StringField()
-    email=StringField()
-    date=StringField()
-    login=StringField()
-    PublicKey=StringField()
-class GithubPeopel(me.Document):
-    user_id=StringField()
-    name=StringField()
-    email=StringField()
-    login=StringField()
-    PublicKey=StringField()
-    starred_at=StringField()
-    node_id=StringField()
